cae158091fcff739c7d855e450ef19e3a90b08f7
KqkaricK <andrew73-42@yandex.ru>
2021-04-25 22:32:16 +0700

    +Первый макет дизайна +Размерность

--- VitchMat_SLAY/Form1.Designer.cs
+++ VitchMat_SLAY/Form1.Designer.cs
@@ -29,13 +29,114 @@ namespace VitchMat_SLAY
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.t_raz = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.darkButton1 = new DarkUI.Controls.DarkButton();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.darkButton2 = new DarkUI.Controls.DarkButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // t_raz
+            // 
+            this.t_raz.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_raz.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_raz.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_raz.Location = new System.Drawing.Point(96, 16);
+            this.t_raz.Name = "t_raz";
+            this.t_raz.Size = new System.Drawing.Size(51, 20);
+            this.t_raz.TabIndex = 0;
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(12, 18);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(78, 13);
+            this.darkLabel1.TabIndex = 1;
+            this.darkLabel1.Text = "Размерность:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 41);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(474, 149);
+            this.dataGridView1.TabIndex = 49;
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Location = new System.Drawing.Point(391, 13);
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton1.Size = new System.Drawing.Size(94, 23);
+            this.darkButton1.TabIndex = 50;
+            this.darkButton1.Text = "Создать СЛАУ";
+            this.darkButton1.Click += new System.EventHandler(this.darkButton1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(11, 242);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(474, 149);
+            this.dataGridView2.TabIndex = 51;
+            // 
+            // darkButton2
+            // 
+            this.darkButton2.Location = new System.Drawing.Point(11, 205);
+            this.darkButton2.Name = "darkButton2";
+            this.darkButton2.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton2.Size = new System.Drawing.Size(94, 23);
+            this.darkButton2.TabIndex = 52;
+            this.darkButton2.Text = "Метод Гаусса";
+            this.darkButton2.Click += new System.EventHandler(this.darkButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(496, 437);
+            this.Controls.Add(this.darkButton2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.darkButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.darkLabel1);
+            this.Controls.Add(this.t_raz);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DarkUI.Controls.DarkTextBox t_raz;
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DarkUI.Controls.DarkButton darkButton1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private DarkUI.Controls.DarkButton darkButton2;
     }
 }
 
--- VitchMat_SLAY/Form1.cs
+++ VitchMat_SLAY/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DarkUI.Forms;
 
 namespace VitchMat_SLAY
 {
-    public partial class Form1 : Form
+    public partial class Form1 : DarkForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void Razmer()
+        {
+            dataGridView1.RowCount = Convert.ToInt32(t_raz.Text);
+            dataGridView1.ColumnCount = Convert.ToInt32(t_raz.Text) + 1;
+        }
+        private void darkButton1_Click(object sender, EventArgs e)
+        {
+            Razmer();
+        }
+
+        private void darkButton2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
